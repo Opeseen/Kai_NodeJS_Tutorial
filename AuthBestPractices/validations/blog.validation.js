0181d4d7895ad7dc1647f04f21f1775126353397
Opeseen <Opeyemi5055@yahoo.com>
2024-01-07 14:03:06 +0100

    Deployed

--- AuthBestPractices/validations/blog.validation.js
+++ AuthBestPractices/validations/blog.validation.js
@@ -3,7 +3,7 @@ const joi = require('joi');
 // Request validation schema
 const createBlogSchema = {
   body: joi.object().keys({
-    title: joi.string().required(),
+    title: joi.string().required().messages({"any.required": "title is required field"}),
     description: joi.string().required(),
   }),
 };
